67a4540491363b482132995559b1a6482f8c1cba
hujuu <1961946+hujuu@users.noreply.github.com>
2023-11-20 00:49:08 +0900

    Create SettingView.swift

---
+++ MyTimer/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  MyTimer
+//
+//  Created by KEN NAKAI on 2023/11/20.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
