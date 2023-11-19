cd61fcbf2b42b19a96a7bd2cb24b4fe99809e377
hujuu <1961946+hujuu@users.noreply.github.com>
2023-11-20 00:04:45 +0900

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by KEN NAKAI on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
